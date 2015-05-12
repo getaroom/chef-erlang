e20d793f2d1d64b32b6acbd4edd28c6a0d620a1d
Morgan Nelson <mnelson@steele-forge.com>
2015-05-12 14:18:51 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license           "Apache 2.0"
 description       "Installs erlang, optionally install GUI tools."
 version           "1.0.0"
 depends           "yum", ">= 0.5.0"
+name               "erlang"
 
 recipe "erlang", "Installs erlang"
 
